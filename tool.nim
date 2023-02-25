0224199253b2ec6e77362ce4caac64dff2871979
Matthew Via <via@matthewvia.info>
2023-02-24 21:45:39 -0500

    show point count

--- tool.nim
+++ tool.nim
@@ -12,6 +12,7 @@ proc fromEpoch(epochTime: uint64): DateTime =
 type Session = object
     start: uint64
     stop: uint64
+    count: Natural
 
 proc getSessions(db: DbConn): seq[Session] =
     let query = "SELECT realtime_ns from points ORDER BY realtime_ns ASC"
@@ -20,9 +21,11 @@ proc getSessions(db: DbConn): seq[Session] =
     var startTime : uint64 = 0
     var last : uint64 = 0
     var startSeen = false
+    var count = 0
 
     for row in db.iterate(query):
         let (time) = row.unpack((uint64, ))
+        count += 1
 
         if not startSeen:
             startSeen = true
@@ -32,16 +35,17 @@ proc getSessions(db: DbConn): seq[Session] =
 
         if time - last > 10 * 1000 * 1000 * 1000:
             # startSeen through last is a session
-            let sesh = Session(start: startTime, stop: last)
+            let sesh = Session(start: startTime, stop: last, count: count)
             sessions.add(sesh)
             startTime = time
             last = time
+            count = 0
             continue
 
         last = time
 
     # Capture any unended session
-    sessions.add(Session(start: startTime, stop: last))
+    sessions.add(Session(start: startTime, stop: last, count: count))
 
     return sessions
 
@@ -53,7 +57,7 @@ proc list(filename: string): void =
     let minutes = (int)(sesh.stop - sesh.start) / (1000000000 * 60)
     let start = fromEpoch(sesh.start)
     let stop = fromEpoch(sesh.stop)
-    echo &"{idx}: {start} - {stop} ({minutes:.1f} minutes)"
+    echo &"{idx}: {start} - {stop} ({sesh.count} points, {minutes:.1f} minutes)"
     idx += 1
 
 dispatchMulti([list])
